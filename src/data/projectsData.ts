803aa0c56ba47f6f2c8b27fc3c4a9d68b9e29ce1
Daniel Holley <danielholley1982@yahoo.com>
2025-05-14 10:29:04 -0500

    Deploy application to Netlify

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -3,7 +3,7 @@ export const projectsData = [
     id: 1,
     title: 'Portfolio Site',
     description: 'A portfolio site to demonstrate my website project abilites',
-    image: 'https://images.pexels.com/photos/5632402/pexels-photo-5632402.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    image: 'https://images.pexels.com/photos/2442888/pexels-photo-2442888.jpeg?cs=srgb&dl=pexels-bemistermister-2442888.jpg&fm=jpg&w=6000&h=4000&_gl=1*ieqovx*_ga*MjE2OTkwMTc4LjE3NDcyMzY0MjA.*_ga_8JE65Q40S6*czE3NDcyMzY0MjAkbzEkZzEkdDE3NDcyMzY0NjgkajAkbDAkaDA.',
     category: 'web',
     technologies: ['React', 'Node.js', 'Supabase'],
     github: 'https://github.com/danielholley/portfolio',
