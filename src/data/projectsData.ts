66b2d03adb5a12a492bf5f0554342fe9cbdc9f70
Daniel Holley <danielholley1982@yahoo.com>
2025-05-14 10:20:03 -0500

    Deploy application to Netlify

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -1,8 +1,8 @@
 export const projectsData = [
   {
     id: 1,
-    title: 'Porfolio Site',
-    description: 'A porfolio site to demonstrate my website project abilites',
+    title: 'Portfolio Site',
+    description: 'A portfolio site to demonstrate my website project abilites',
     image: 'https://images.pexels.com/photos/5632402/pexels-photo-5632402.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
     category: 'web',
     technologies: ['React', 'Node.js', 'Supabase'],
